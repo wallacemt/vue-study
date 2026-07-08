9fcd3d6068e3b2aee24fc51ab09d09ad22c3b020
Wallace Santana <wallacesantanak0@gmail.com>
2026-07-07 21:02:00 -0300

    feat(./lesson01): initial study vue app

---
+++ lesson01/first-contact/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Button from './components/Button.vue'
+</script>
+
+<template>
+  <main>
+    <h1 >Welcome to Vue 3 with TypeScript!</h1>
+    
+    <Button />
+  </main>
+
+</template>
